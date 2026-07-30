f09bd7344661b56639cec2f2d86819df93aa149e
vinhdau088-cloud <vinhdau088@gmail.com>
2026-07-30 22:28:16 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
-
+import SearchBar from "@/components/SearchBar";
 export default function Home() {
   return (
     <>
